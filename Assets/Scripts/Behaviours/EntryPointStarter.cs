148f29d5d3e40376a827246c6be3a3fa1aee8bfa
shelhelix <ultrashel@gmail.com>
2020-11-29 16:39:37 +0700

    inited empty Unity 2019.4.8f1 project

---
+++ Assets/Scripts/Behaviours/EntryPointStarter.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace SecretSantaGameJam2020.Behaviours {
+	public class EntryPointStarter : MonoBehaviour {
+		void Start() {
+			//TODO: Add start gameplay initialization
+		}
+	}
+}
