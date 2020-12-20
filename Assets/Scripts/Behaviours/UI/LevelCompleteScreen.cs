70f2c0b9b82fcb2ddbb2e156fb6ed4362e37e307
shelhelix <ultrashel@gmail.com>
2020-12-20 21:29:43 +0700

    added "restart level" buttons to the UI screens

--- Assets/Scripts/Behaviours/UI/LevelCompleteScreen.cs
+++ Assets/Scripts/Behaviours/UI/LevelCompleteScreen.cs
@@ -1,4 +1,8 @@
-﻿using SecretSantaGameJam2020.Behaviours.Common;
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+using SecretSantaGameJam2020.Behaviours.Common;
 using SecretSantaGameJam2020.Utils.CustomAttributes;
 
 using DG.Tweening;
@@ -9,15 +13,27 @@ namespace SecretSantaGameJam2020.Behaviours.UI {
         [NotNull] public ScreenTransitionController TransitionController;
         
         [NotNull] public TMP_Text Text;
+        [NotNull] public TMP_Text GoToNextLevelText;
+        [NotNull] public Button   Button;
         
         public float TransitionTime    = 3f;
+        
         public float TextAppearingTime = 0.5f;
+        public float TextEndScale      = 1.5f;
+        
+        public float RestartTextStartScale    = 0.5f;
+        public float RestartTextAppearingTime = 0.2f;
         
         Sequence _activeSequence;
         
         public void Show() {
             gameObject.SetActive(true);
-            Text.alpha = 0f;
+            
+            Button.onClick.AddListener(() => {
+                Button.onClick.RemoveAllListeners();
+                GoToNextLevelText.text = "Loading...";
+                SceneManager.LoadSceneAsync("EntryPoint");
+            });
             _activeSequence = TransitionController.CloseTransition(TransitionTime).Append(CreateAppearingTextSeq());
         }
 
@@ -28,7 +44,15 @@ namespace SecretSantaGameJam2020.Behaviours.UI {
         }
 
         Sequence CreateAppearingTextSeq() {
-            return DOTween.Sequence().Append(Text.DOFade(1f, TextAppearingTime));
+            Text.alpha              = 0f;
+            GoToNextLevelText.alpha = 0f;
+            Text.transform.localScale              = Vector3.one;
+            GoToNextLevelText.transform.localScale = Vector3.one * RestartTextStartScale ;
+            return DOTween.Sequence()
+                .Append(Text.DOFade(1f, TextAppearingTime))
+                .Join(Text.transform.DOScale(Vector3.one * TextEndScale, TextAppearingTime))
+                .Append(GoToNextLevelText.DOFade(1f, RestartTextAppearingTime))
+                .Join(GoToNextLevelText.transform.DOScale(Vector3.one, RestartTextAppearingTime));
         }
     }
 }
--- Assets/Scripts/Behaviours/UI/PlayerDeadScreen.cs
+++ Assets/Scripts/Behaviours/UI/PlayerDeadScreen.cs
@@ -1,4 +1,6 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 using SecretSantaGameJam2020.Behaviours.Common;
 using SecretSantaGameJam2020.Utils.CustomAttributes;
@@ -11,28 +13,46 @@ namespace SecretSantaGameJam2020.Behaviours.UI {
         [NotNull] public ScreenTransitionController TransitionController;
         
         [NotNull] public TMP_Text Text;
+        [NotNull] public TMP_Text RestartText;
+        [NotNull] public Button   Button;
         
         public float TransitionTime    = 3f;
+        
         public float TextAppearingTime = 0.5f;
         public float TextEndScale      = 1.5f;
+        
+        public float RestartTextStartScale    = 0.5f;
+        public float RestartTextAppearingTime = 0.2f;
         
         Sequence _activeSequence;
         
         public void Show() {
             gameObject.SetActive(true);
             _activeSequence = TransitionController.CloseTransition(TransitionTime).Append(CreateAppearingTextSeq());
+            Button.onClick.AddListener(() => {
+                Button.onClick.RemoveAllListeners();
+                RestartText.text = "Loading...";
+                SceneManager.LoadSceneAsync("EntryPoint");
+            });
         }
 
         public void Hide() {
+            Button.onClick.RemoveAllListeners();
             _activeSequence?.Kill();
             _activeSequence = null;
             gameObject.SetActive(false);
         }
 
         Sequence CreateAppearingTextSeq() {
-            Text.alpha = 0f;
-            Text.transform.localScale = Vector3.one;
-            return DOTween.Sequence().Append(Text.DOFade(1f, TextAppearingTime)).Join(Text.transform.DOScale(Vector3.one * TextEndScale, TextAppearingTime));
+            Text.alpha        = 0f;
+            RestartText.alpha = 0f;
+            Text.transform.localScale        = Vector3.one;
+            RestartText.transform.localScale = Vector3.one * RestartTextStartScale ;
+            return DOTween.Sequence()
+                .Append(Text.DOFade(1f, TextAppearingTime))
+                .Join(Text.transform.DOScale(Vector3.one * TextEndScale, TextAppearingTime))
+                .Append(RestartText.DOFade(1f, RestartTextAppearingTime))
+                .Join(RestartText.transform.DOScale(Vector3.one, RestartTextAppearingTime));
         }
     }
 }
