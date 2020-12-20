3b48dc2de4eeef6d1acad2b62c3b092cecdfe177
shelhelix <ultrashel@gmail.com>
2020-12-20 13:39:04 +0700

    added random room decorations

--- Assets/Scripts/Behaviours/Rooms/Room.cs
+++ Assets/Scripts/Behaviours/Rooms/Room.cs
@@ -1,4 +1,8 @@
-﻿using SecretSantaGameJam2020.Behaviours.Common;
+﻿using UnityEngine;
+
+using System.Collections.Generic;
+
+using SecretSantaGameJam2020.Behaviours.Common;
 using SecretSantaGameJam2020.Utils.CustomAttributes;
 
 namespace SecretSantaGameJam2020.Behaviours.Rooms {
@@ -7,12 +11,28 @@ namespace SecretSantaGameJam2020.Behaviours.Rooms {
 		[NotNull] public Teleport LeftTeleport;
 		[NotNull] public Teleport RightTeleport;
 		[NotNull] public Teleport BottomTeleport;
+
+		[NotNull] public List<GameObject> PossibleDecorations;
 		
 		public virtual void Init( bool isLeftDoorOpened, bool isRightDoorOpened, bool isUpperDoorOpened, bool isBottomDoorOpened) {
 			UpperTeleport.Init(isUpperDoorOpened);
 			LeftTeleport.Init(isLeftDoorOpened);
 			RightTeleport.Init(isRightDoorOpened);
 			BottomTeleport.Init(isBottomDoorOpened);
+			InitRandomDecorations();
+		}
+
+		void InitRandomDecorations() {
+			if ( PossibleDecorations.Count == 0 ) {
+				return;
+			}
+			var decorationsToEnable = Random.Range(0, PossibleDecorations.Count);
+			var decorationsCopy = new List<GameObject>(PossibleDecorations);
+			for ( var i = 0; i < decorationsToEnable; i++ ) {
+				var randomDecorationIndex = Random.Range(0, decorationsCopy.Count);
+				decorationsCopy[randomDecorationIndex].SetActive(true);
+				decorationsCopy.RemoveAt(randomDecorationIndex);
+			}
 		}
 	}
 }
